d9cd1c22cc888d31057a5d5e400c8504f355a499
Chaoming Chen <chaoming.edc@gmail.com>
2013-03-10 23:27:15 +0800

    Test new Datagrid.
    Signed-off-by: Chaoming Chen <chaoming.edc@gmail.com>

--- EnvManWF/EnvManWPF/MainWindow.xaml.cs
+++ EnvManWF/EnvManWPF/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,17 +15,42 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using MahApps.Metro.Controls;
+using SFC.EnvMan;
 
 namespace EnvManWPF
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : MetroWindow
-	{
-		public MainWindow()
-		{
-			InitializeComponent();
-		}
-	}
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        private EnvVarManager variableManger = new EnvVarManager();        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadEnvironmentVars();
+        }
+
+        private void LoadEnvironmentVars()
+        {
+            LoadEnvironmentVariables(this.dgv, EnvironmentVariableTarget.User);
+        }
+
+        /// <summary>
+        /// Loads the environment variables.
+        /// </summary>
+        /// <param name="dgv">The Data Grid View.</param>
+        /// <param name="target">The target.</param>
+        private void LoadEnvironmentVariables(
+            DataGrid dgv, EnvironmentVariableTarget target)
+        {
+            EnvVarValueValidator validator = new EnvVarValueValidator();
+
+            IDictionary environmentVariables
+                = this.variableManger.GetEnvVariables(target);
+
+            dgv.ItemsSource = environmentVariables;
+        }
+    }
 }
